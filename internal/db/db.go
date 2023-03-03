e747f23bcdd592c7bf380ff5022fa5c924ad3382
Georgy Grechko <georgre4ko@ya.ru>
2023-03-03 22:42:52 +0300

    Фикс юнит-тестов

--- internal/db/db.go
+++ internal/db/db.go
@@ -4,15 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/go-park-mail-ru/2023_1_PracticalDev/internal/config"
 	"github.com/go-park-mail-ru/2023_1_PracticalDev/internal/log"
 	_ "github.com/lib/pq"
 )
 
 func New(logger log.Logger) (*sql.DB, error) {
 	params := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		"db", "5432", "pickpin", "pickpindb", "pickpinpswd", "disable")
+		config.Get("PGHOST"), config.Get("PGPORT"), config.Get("POSTGRES_USER"), config.Get("POSTGRES_DB"), config.Get("POSTGRES_PASSWORD"), config.Get("POSTGRES_SSL"))
 
-	logger.Info("Connecting to db...")
+	logger.Info("Connecting to db...", params)
 	db, err := sql.Open("postgres", params)
 	if err != nil {
 		logger.Error("Failed to create DB connection, ", err.Error())
