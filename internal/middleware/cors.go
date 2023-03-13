2d8d5aff6d2ef47bcd915ed3d2bfa6a32fac8637
SlavaShagalov <slavikshagalov@gmail.com>
2023-03-14 00:22:42 +0300

    Вернул CORS обработчик к прежнему варианту, т.к. новый вариант оказался нерабочим.

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/go-park-mail-ru/2023_1_PracticalDev/internal/router"
@@ -34,16 +33,12 @@ func OptionsHandler(w http.ResponseWriter, r *http.Request) {
 
 func CorsChecker(handler router.Handler) router.Handler {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) error {
-		origin := r.Header.Get("Origin")
-		secFetchSite := r.Header.Get("Sec-Fetch-Site")
-		if secFetchSite == "same-origin" || secFetchSite == "none" {
-			return handler(w, r, p)
-		} else if _, allowed := allowedOrigins[origin]; allowed {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			return handler(w, r, p)
+		origin := mainOrigin
+		gotOrigin := r.Header.Get("Origin")
+		if _, allowed := allowedOrigins[gotOrigin]; allowed {
+			origin = gotOrigin
 		}
-		w.Header().Set("Access-Control-Allow-Origin", mainOrigin)
-		w.WriteHeader(http.StatusForbidden)
-		return errors.New("CORS: origin not allowed")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return handler(w, r, p)
 	}
 }
