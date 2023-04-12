db29c26695846bc4b7638742a0c1c17462e25c2c
Georgy Grechko <georgre4ko@ya.ru>
2023-04-13 02:45:53 +0300

    Небольшой фикс тестовых данных и nginx конфига

--- scripts/migrations/test_data.sql
+++ scripts/migrations/test_data.sql
@@ -1,10 +1,10 @@
-INSERT INTO users(username, hashed_password, name, email, account_type)
+INSERT INTO users(username, hashed_password, name, email, account_type, profile_image)
 VALUES ('geogreck', '$2a$10$A4Ab/cuy/oLNvm4VxGoO/ezKL.fiew5e.eKTkUOWIVxoBh8XFO4lS', 'George', 'geogreck@vk.com',
-        'personal'),
-       ('kirill', '$2a$10$A4Ab/cuy/oLNvm4VxGoO/ezKL.fiew5e.eKTkUOWIVxoBh8XFO4lS', 'Kirill', 'figma@vk.com', 'personal'),
-       ('slava', '$2a$10$A4Ab/cuy/oLNvm4VxGoO/ezKL.fiew5e.eKTkUOWIVxoBh8XFO4lS', 'Slava', 'iu7@vk.com', 'personal'),
+        'personal', 'https://i.pinimg.com/564x/ce/e3/01/cee3011f3e19de4377dbf98f397c027b.jpg'),
+       ('kirill', '$2a$10$A4Ab/cuy/oLNvm4VxGoO/ezKL.fiew5e.eKTkUOWIVxoBh8XFO4lS', 'Kirill', 'figma@vk.com', 'personal', 'https://i.pinimg.com/564x/ce/e3/01/cee3011f3e19de4377dbf98f397c027b.jpg'),
+       ('slava', '$2a$10$A4Ab/cuy/oLNvm4VxGoO/ezKL.fiew5e.eKTkUOWIVxoBh8XFO4lS', 'Slava', 'iu7@vk.com', 'personal', 'https://i.pinimg.com/564x/ce/e3/01/cee3011f3e19de4377dbf98f397c027b.jpg'),
        ('evgenii', '$2a$10$A4Ab/cuy/oLNvm4VxGoO/ezKL.fiew5e.eKTkUOWIVxoBh8XFO4lS', 'evgenii', 'test@vk.com',
-        'personal');
+        'personal', 'https://i.pinimg.com/564x/ce/e3/01/cee3011f3e19de4377dbf98f397c027b.jpg');
 
 INSERT INTO boards(name, privacy, user_id)
 VALUES ('Notes', 'public', 1),
@@ -15,13 +15,11 @@ VALUES ('Notes', 'public', 1),
        ('ToDo', 'secret', 3);
 
 INSERT INTO pins(title, author_id, media_source)
-VALUES ('Road', 1, 'https://wg.grechkogv.ru/assets/pet7.webp'),
-       ('Ice', 1, 'https://wg.grechkogv.ru/assets/armorChest4.webp'),
-       ('Future', 1, 'https://wg.grechkogv.ru/assets/pet6.webp'),
-       ('Color', 2, 'https://wg.grechkogv.ru/assets/pet8.webp'),
-       ('Shops', 3, 'https://i.pinimg.com/564x/2f/93/56/2f9356b9346e82c14bf286c6a107bc7a.jpg'),
-       ('Shops', 3, 'https://i.pinimg.com/564x/32/ff/71/32ff717c3cd3bd3d1886c775b59f0769.jpg'),
-       ('Shops', 3, 'https://i.pinimg.com/564x/ce/e3/01/cee3011f3e19de4377dbf98f397c027b.jpg'),
+       ('Ice', 1, 'https://pickpin.hb.bizmrg.com/68ed77e8-9002-491d-bd3d-d36938e054b8.jpg'),
+       ('Future', 1, 'https://pickpin.hb.bizmrg.com/7c0a9457-6ea6-4c26-b8fd-4d7c0276c568.jpg'),
+       ('Shops', 1, 'https://i.pinimg.com/564x/2f/93/56/2f9356b9346e82c14bf286c6a107bc7a.jpg'),
+       ('Shops', 2, 'https://i.pinimg.com/564x/32/ff/71/32ff717c3cd3bd3d1886c775b59f0769.jpg'),
+       ('Shops', 2, 'https://i.pinimg.com/564x/ce/e3/01/cee3011f3e19de4377dbf98f397c027b.jpg'),
        ('Shops', 3, 'https://i.pinimg.com/564x/a6/ba/55/a6ba553df2a0c0f3894ef328a86fb373.jpg'),
        ('Shops', 3, 'https://i.pinimg.com/564x/43/2d/3b/432d3b28d1661439245422e9218ffcce.jpg'),
        ('School', 4, 'https://i.pinimg.com/564x/98/9d/3f/989d3f5c158dcac7ca4d115bff866d84.jpg');
