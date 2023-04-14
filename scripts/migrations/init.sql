44ef5d7f52fd6d922ce46e1ae0503def9074a04a
SlavaShagalov <slavikshagalov@gmail.com>
2023-04-14 18:12:42 +0300

    Добавил ON DELETE CASCADE для boards_pins таблицы.

--- scripts/migrations/init.sql
+++ scripts/migrations/init.sql
@@ -36,9 +36,9 @@ CREATE TABLE IF NOT EXISTS pins
 
 CREATE TABLE IF NOT EXISTS boards_pins
 (
-    id       serial NOT NULL PRIMARY KEY,
-    board_id int    NOT NULL,
-    pin_id   int    NOT NULL
+    board_id int REFERENCES boards (id) ON DELETE CASCADE,
+    pin_id   int REFERENCES pins (id) ON DELETE CASCADE,
+    PRIMARY KEY (board_id, pin_id)
 );
 
 CREATE TABLE IF NOT EXISTS comments
@@ -62,25 +62,6 @@ ALTER TABLE ONLY boards
     ADD CONSTRAINT fk_boards_user_id
         FOREIGN KEY (user_id)
             REFERENCES users (id);
-
-ALTER TABLE ONLY boards_pins
-    ADD CONSTRAINT fk_pins_board_id
-        FOREIGN KEY (board_id)
-            REFERENCES boards (id);
-
-ALTER TABLE ONLY boards_pins
-    ADD CONSTRAINT fk_board_pins_id
-        FOREIGN KEY (pin_id)
-            REFERENCES pins (id);
-
-ALTER TABLE ONLY boards_pins
-    DROP CONSTRAINT fk_board_pins_id,
-    ADD CONSTRAINT fk_board_pins_id
-        FOREIGN KEY (pin_id) REFERENCES pins (id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY boards_pins
-    ADD CONSTRAINT unique_id
-        UNIQUE (board_id, pin_id);
 
 ALTER TABLE ONLY pins
     ADD CONSTRAINT fk_pins_author_id
