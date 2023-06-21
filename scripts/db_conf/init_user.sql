f4864fec02704026ff66d8b93820e2bd93b37063
Kirill Kiselev <lirikk285@mail.ru>
2023-06-21 22:42:50 +0300

    fix init user

--- scripts/db_conf/init_user.sql
+++ scripts/db_conf/init_user.sql
@@ -2,16 +2,16 @@ CREATE USER pickpin_main WITH PASSWORD 'pickpinpswd';
 CREATE USER pickpin_search WITH PASSWORD 'pickpinpswd';
 
 GRANT SELECT, UPDATE, INSERT, DELETE ON
-    users,
-    boards,
-    pins,
+    users, users_id_seq,
+    boards, boards_id_seq,
+    pins, pins_id_seq,
     pin_likes,
     boards_pins,
-    comments,
+    comments, comments_id_seq,
     followings,
-    chats,
-    messages,
-    notifications,
+    chats, chats_id_seq,
+    messages, messages_id_seq,
+    notifications, notifications_id_seq,
     new_pin_notifications,
     new_like_notifications,
     new_comment_notifications,
